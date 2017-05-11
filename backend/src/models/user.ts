66c16bb363ac658e22e069c3ef7e86d2009fcd6e
Michael Nielsen <mcl@codewizards.dk>
2017-05-11 20:22:34 +0200

    Adding pw field

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -4,13 +4,15 @@ interface IUser {
     first_name: string;
     last_name: string;
     email: string;
+    password: string
 }
 
 interface IUserModel extends IUser, mongoose.Document {}
 const userSchema: mongoose.Schema = new mongoose.Schema({
-    first_name: String,
-    last_name: String,
-    email: String,
+    first_name: {type: String, required: true},
+    last_name: {type: String, required: true},
+    email: {type: String, required: true, index: { unique: true }},
+    password: {type: String, required: true},
 });
 
 const User: mongoose.Model<IUserModel> = mongoose.model<IUserModel>("User", userSchema);
